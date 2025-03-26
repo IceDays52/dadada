4e2a9b761445099de8bfe88548859cf72c26f86a
Arek <icedays222@gmail.com>
2025-03-26 16:56:12 +0100

    codewars

--- Zadaniapy/dada.py
+++ Zadaniapy/dada.py
@@ -1,2 +1,11 @@
-print("Witam")
-def jakak(s):
+
+
+def DNA_strand(dna):
+ converter = { 'A': 'T', 'T': 'A', 'C': 'G', 'G' : 'C'}
+  
+ return ''.capitalize().join([converter[i] for i in dna])
+
+skibidi = "ATTGC"
+
+for dna in skibidi:
+    print(DNA_strand(dna))
